58b58862a8a8c79c31d2b785154b2ae80879da87
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-06-06 15:16:53 +0330

    RazorNukePageService

---
+++ RazorNuke/Services/Implementation/RazorNukePageService.cs
@@ -0,0 +1,15 @@
+﻿using RazorNuke.DbContext;
+
+namespace RazorNuke.Services.Implementation
+{
+    public class RazorNukePageService
+    {
+
+
+        protected readonly RDbContext _context;
+        public RazorNukePageService(RDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
