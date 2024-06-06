3f0978441826c313865e13b27e9bd27d5180632f
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-06-06 21:27:48 +0330

    page CRUD

--- RazorNuke/Services/IRazorNukePageService.cs
+++ RazorNuke/Services/IRazorNukePageService.cs
@@ -6,6 +6,27 @@ namespace RazorNuke.Services
     public interface IRazorNukePageService
     {
         /// <summary>
+        /// add new page
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        Task<RServiceResult<RazorNukePage?>> AddAsync(RazorNukePage page);
+
+        /// <summary>
+        /// update page
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        Task<RServiceResult<RazorNukePage?>> UpdateAsync(RazorNukePage page);
+
+        /// <summary>
+        /// delete page
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<RServiceResult<bool>> DeleteAsync(int id);
+
+        /// <summary>
         /// get page children
         /// </summary>
         /// <param name="parentId"></param>
--- RazorNuke/Services/Implementation/RazorNukePageService.cs
+++ RazorNuke/Services/Implementation/RazorNukePageService.cs
@@ -9,6 +9,66 @@ namespace RazorNuke.Services.Implementation
     {
 
         /// <summary>
+        /// add new page
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        public async Task<RServiceResult<RazorNukePage?>> AddAsync(RazorNukePage page)
+        {
+            try
+            {
+                _context.Add(page);
+                await _context.SaveChangesAsync();
+                return new RServiceResult<RazorNukePage?>(page);
+            }
+            catch (Exception exp)
+            {
+                return new RServiceResult<RazorNukePage?>(null, exp.ToString());
+            }
+        }
+
+        /// <summary>
+        /// update page
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        public async Task<RServiceResult<RazorNukePage?>> UpdateAsync(RazorNukePage page)
+        {
+            try
+            {
+                var dbPage = await _context.Pages.Where(p => p.Id == page.Id).SingleAsync();
+                _context.Entry(dbPage).CurrentValues.SetValues(dbPage);
+                _context.Update(dbPage);
+                await _context.SaveChangesAsync();
+                return new RServiceResult<RazorNukePage?>(dbPage);
+            }
+            catch (Exception exp)
+            {
+                return new RServiceResult<RazorNukePage?>(null, exp.ToString());
+            }
+        }
+
+        /// <summary>
+        /// delete page
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<RServiceResult<bool>> DeleteAsync(int id)
+        {
+            try
+            {
+                var dbPage = await _context.Pages.Where(p => p.Id == id).SingleAsync();
+                _context.Remove(dbPage);
+                await _context.SaveChangesAsync();
+                return new RServiceResult<bool>(true);
+            }
+            catch (Exception exp)
+            {
+                return new RServiceResult<bool>(false, exp.ToString());
+            }
+        }
+
+        /// <summary>
         /// get page children
         /// </summary>
         /// <param name="parentId"></param>
