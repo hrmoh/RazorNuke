068c456beaf886fcfa0eb287fb59e411f2f176c2
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-07-02 15:32:40 +0330

    RazorNukeMenuItem

---
+++ RazorNuke/Models/ViewModels/RazorNukeMenuItem.cs
@@ -0,0 +1,35 @@
+﻿namespace RazorNuke.Models.ViewModels
+{
+    public class RazorNukeMenuItem
+    {
+        /// <summary>
+        /// page id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// page order
+        /// </summary>
+        public int PageOrder { get; set; }
+
+        /// <summary>
+        /// title in menu
+        /// </summary>
+        public string TitleInMenu { get; set; }
+
+        /// <summary>
+        /// full page url including parents urls
+        /// </summary>
+        public required string FullUrl { get; set; }
+
+        /// <summary>
+        /// selected
+        /// </summary>
+        public bool Selected { get; set; }
+
+        /// <summary>
+        /// children
+        /// </summary>
+        public RazorNukeMenuItem[] Children { get; set; }
+    }
+}
